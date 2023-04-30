6129aaf97d4f6dba746052c1d1453d112ac314db
Laddge <dev.laddge@gmail.com>
2023-04-30 06:42:53 +0000

    add items

---
+++ src/assets/items.ts
@@ -0,0 +1,208 @@
+interface Item {
+  name: string
+  price: number
+  img: string
+}
+
+export const items: Item[] = [
+  {
+    name: 'スニーカー',
+    price: 6150,
+    img: 'https://1.bp.blogspot.com/-ZvxBWXjruCU/UkJMDLIMRwI/AAAAAAAAYQQ/FrNA1aboBX0/s800/shoes_sneaker.png',
+  },
+  {
+    name: 'シャープペンシル',
+    price: 495,
+    img: 'https://4.bp.blogspot.com/-PIk5HJ74ias/W9RcJlC4w1I/AAAAAAABPoo/ET2HuWKYSNs_qWuEjfa15Tn2EXTs4yYxACLcBGAs/s800/bunbougu_shappen.png',
+  },
+  {
+    name: '電気ポット',
+    price: 4067,
+    img: 'https://1.bp.blogspot.com/-JHKujeQllsI/VfS6-Km16_I/AAAAAAAAxWU/9-iHiy9yiXA/s800/cooking_denki_pot.png',
+  },
+  {
+    name: 'お米',
+    price: 3398,
+    img: 'https://2.bp.blogspot.com/-c91VRhTN_cI/WUdYqqYdKAI/AAAAAAABE-M/rJbxEZDVOT80d-6IyIs8yt6Ji6JKgoV6wCLcBGAs/s800/food_kome_pack_big.png',
+  },
+  {
+    name: 'リュックサック',
+    price: 7980,
+    img: 'https://4.bp.blogspot.com/-7d9gtgZP8zk/UsZtOUSG02I/AAAAAAAAczw/OkFZohxAjcI/s800/rucksack_backpack.png',
+  },
+  {
+    name: '漫画',
+    price: 498,
+    img: 'https://3.bp.blogspot.com/-Bd3Nw4RAy-c/Wj4IX6sxskI/AAAAAAABJKk/1SWTrkq-07Yut881iPXlIdjKCt4LMSf_gCLcBGAs/s800/entertainment_comic.png',
+  },
+  {
+    name: 'キャビア',
+    price: 12960,
+    img: 'https://2.bp.blogspot.com/-w0xWFyUstf8/U00KDRQJX-I/AAAAAAAAfNc/by6T4wrt45k/s800/food_caviar.png',
+  },
+  {
+    name: '椅子',
+    price: 5980,
+    img: 'https://3.bp.blogspot.com/-7CfCfwzx_Fw/WGCxQ81IuRI/AAAAAAABApM/lQTJsPkDgAMXqOPaysRZQ8fkpj7N8XyrgCLcB/s800/chair_officechair.png',
+  },
+  {
+    name: 'ヘッドホン',
+    price: 6500,
+    img: 'https://3.bp.blogspot.com/-oMG_B_VsyO4/U401DSJbD-I/AAAAAAAAg8w/Mb1E93t9vBQ/s800/head_phone.png',
+  },
+  {
+    name: '下駄',
+    price: 8030,
+    img: 'https://2.bp.blogspot.com/-BAqceIkSC5w/UkJMBIOAE8I/AAAAAAAAYPk/J47i3SByTSQ/s800/shoes_geta.png',
+  },
+  {
+    name: 'かぜ薬',
+    price: 1446,
+    img: 'https://2.bp.blogspot.com/-57P4j4ba84E/VyNdckfKDVI/AAAAAAAA6No/00EjX61RCQwiFfvQjglB7M55X2xsO65MQCLcB/s800/medicine_kaze.png',
+  },
+  {
+    name: 'ゲーム課金',
+    price: 10000,
+    img: 'https://1.bp.blogspot.com/-Hke6uvZRFJc/VIhO_y0nMVI/AAAAAAAApks/TzJAnv82lKI/s800/game_kakin.png',
+  },
+  {
+    name: 'カラオケ',
+    price: 1100,
+    img: 'https://1.bp.blogspot.com/-qb84yhHlsVM/YLw8jz3cD2I/AAAAAAABd2A/idpF2Q_-JBsHMOUDmTbgyl1o_G5FujiqwCNcBGAsYHQ/s879/music_karaoke_denmoku.png',
+  },
+  {
+    name: 'くつ下',
+    price: 390,
+    img: 'https://4.bp.blogspot.com/-RSOpHGmNPXA/UYiNzeOkQnI/AAAAAAAARTM/G0j7oN1zFms/s800/cloth_kutsushita.png',
+  },
+  {
+    name: 'シナノゴールド',
+    price: 6980,
+    img: 'https://1.bp.blogspot.com/-rb5mSYSN8pA/X6tmegQM2ZI/AAAAAAABcLw/_-n5UvfxhJItVJnKRrycKPShVDsxStrjACNcBGAsYHQ/s755/fruit_apple_yellow.png',
+  },
+  {
+    name: 'コーラ',
+    price: 189,
+    img: 'https://1.bp.blogspot.com/-3wo33nLxIKw/X1LskM84NQI/AAAAAAABa_I/D7QLbbJYqOsyI26PwlE23Dl6Dy-CzM8rQCNcBGAsYHQ/s1600/drink_cola_petbottle.png',
+  },
+  {
+    name: 'ゲームソフト',
+    price: 5780,
+    img: 'https://4.bp.blogspot.com/-A9hivhWvHM8/Vw5KcNOtwlI/AAAAAAAA5rw/E7nwnHHBfowA2MG2GpPi_tXDS1dfZHfHACLcB/s800/game_tsumige.png',
+  },
+  {
+    name: '草履',
+    price: 3880,
+    img: 'https://1.bp.blogspot.com/-lJFvmg2XYjk/VsG0A-13EbI/AAAAAAAA4Bw/6RD-5RA99OA/s800/zouri_man.png',
+  },
+  {
+    name: '誕生日プレゼント',
+    price: 110,
+    img: 'https://1.bp.blogspot.com/-RpmsxIr4dPg/UYG_s7wZ59I/AAAAAAAARCo/OIOGEwufm1g/s800/birthday_present.png',
+  },
+  {
+    name: 'スパナ',
+    price: 1458,
+    img: 'https://1.bp.blogspot.com/-rWJYIR40EPs/XkZczrEujVI/AAAAAAABXQA/ZZz_GtkjBJo8llpbW4nOKi18VreCoyivwCNcBGAsYHQ/s1600/dougu_spanner.png',
+  },
+  {
+    name: 'ディスクグラインダー',
+    price: 11598,
+    img: 'https://1.bp.blogspot.com/-62DKIlLxatI/XTPoCwvR_bI/AAAAAAABTvg/rwsxRW--kLgZXAUVN5B5fsjh-leWxW-hACLcBGAs/s800/dougu_machine_disk_grinder.png',
+  },
+  {
+    name: 'ジンバル',
+    price: 3980,
+    img: 'https://1.bp.blogspot.com/-4ShIM0wL4kg/YHDkQtEO2nI/AAAAAAABdmw/dG7WYbkOyx8RLnQZyJwRYviinlsqsN8LQCNcBGAsYHQ/s821/smartphone_gimbal.png',
+  },
+  {
+    name: 'さすまた',
+    price: 7680,
+    img: 'https://1.bp.blogspot.com/-lBwjcbFpUC8/X7zMGDag4KI/AAAAAAABcYA/UfxtLUmMtG4yghNnMtg6NHhI912d2jo_QCNcBGAsYHQ/s763/buki_sasumata.png',
+  },
+  {
+    name: 'バーベキューコンロ',
+    price: 9980,
+    img: 'https://1.bp.blogspot.com/-Bw5ZckDs9X8/XdttVGl2K5I/AAAAAAABWG4/ySICN6pGG68DXOA3iGg6OehjhfY4UYzwACNcBGAsYHQ/s1600/cooking_camp_bbq_grill.png',
+  },
+  {
+    name: 'コインケース',
+    price: 110,
+    img: 'https://4.bp.blogspot.com/-Ife4UyJMcKk/XAY5_dw8j0I/AAAAAAABQgg/zWFTiubCd18-XCgoiZ0l2nno0NaPSfjuACLcBGAs/s800/money_coin_case.png',
+  },
+  {
+    name: 'ホットサンドメーカー',
+    price: 6600,
+    img: 'https://3.bp.blogspot.com/-fFhD7SizEQU/Ws2uqCV0dwI/AAAAAAABLQE/DtYrk-gD_Kwyk6UqrBpJHCcDzbghHG4QACLcBGAs/s800/cooking_hotsand_sandwich_pan.png',
+  },
+  {
+    name: '高級な水',
+    price: 12600,
+    img: 'https://2.bp.blogspot.com/-sq7JgJF8z74/VJF-cgx9MHI/AAAAAAAAprQ/O5RHXm_J4NM/s800/bottle_water.png',
+  },
+  {
+    name: '剪定はさみ',
+    price: 2954,
+    img: 'https://3.bp.blogspot.com/-CO4VWVGbqQo/WlGnzeDwDvI/AAAAAAABJeU/fsJgJvK8m80d_myAegQbCAMOGZLJO2T0wCLcBGAs/s800/gardening_sentei_hasami.png',
+  },
+  {
+    name: '10円ガム',
+    price: 10,
+    img: 'https://2.bp.blogspot.com/-q6jYC0xIPvQ/WGCxU6HCvGI/AAAAAAABAqo/LZLHYOE6atATM3T1phG76jv4vNii5_GLACLcB/s800/sweets_itagum.png',
+  },
+  {
+    name: '技術書',
+    price: 2640,
+    img: 'https://4.bp.blogspot.com/-ojwhTWkQ5nA/WzC9ZpDx27I/AAAAAAABM4U/d5T0pFDbygU5pon0Qh_vXFA8lhlroh6-ACLcBGAs/s800/book_gijutsusyo_it_set.png',
+  },
+  {
+    name: 'スマートスピーカー',
+    price: 6050,
+    img: 'https://2.bp.blogspot.com/-G1txY8jRqZ8/WTJrKUhw3eI/AAAAAAABElo/kxrLh8TQNq8qyKR4nKn0Xx-IPpuszGqOQCLcB/s800/ai_smart_speaker.png',
+  },
+  {
+    name: 'デート代',
+    price: 5100,
+    img: 'https://3.bp.blogspot.com/-4TEoxm0L3fM/VoX5K5zU6CI/AAAAAAAA2S0/kEyNKzeJLHI/s800/couple_date.png',
+  },
+  {
+    name: 'ガスマスク',
+    price: 4538,
+    img: 'https://4.bp.blogspot.com/-JlHs2AAcuyA/Ur1HgiodHzI/AAAAAAAAchk/cEMB0yp7qqI/s800/gas_mask.png',
+  },
+  {
+    name: 'もやし',
+    price: 20,
+    img: 'https://1.bp.blogspot.com/-Mg_bPzvfARs/X7zMMXm5MwI/AAAAAAABcZs/w1r2ibtWh6EirjFEcJPaYiNeyZlGqT8jgCNcBGAsYHQ/s672/vegetable_moyashi_pack.png',
+  },
+  {
+    name: '有線イヤホン',
+    price: 3530,
+    img: 'https://3.bp.blogspot.com/-ei0bpBP841A/Vz_xIIZnpaI/AAAAAAAA6v0/R_dVegvPc3cxm-tb17Grk4fgc_mjgcakgCLcB/s800/music_earphone_canal.png',
+  },
+  {
+    name: 'ワイヤレスイヤホン',
+    price: 12980,
+    img: 'https://4.bp.blogspot.com/-Z1XddY8SRZ0/WK_ZhscW7mI/AAAAAAABCDE/WoAT2IPfI-sVCbnsJL4bKgr1M5m1L4FdACLcB/s800/music_earphone_true_wireless_case.png',
+  },
+  {
+    name: '焼酎',
+    price: 2600,
+    img: 'https://1.bp.blogspot.com/-B638RXlpBo8/X5OcRbFcLrI/AAAAAAABb7k/67BUFLOsu2EhOwpacuxeNiJTrjT6mtYVgCNcBGAsYHQ/s834/drink_syouchu_bottle.png',
+  },
+  {
+    name: '消しゴム',
+    price: 110,
+    img: 'https://2.bp.blogspot.com/-e77cr6l0sR4/UOFKBMw2R4I/AAAAAAAAKDg/6eTaQgsBGZQ/s1600/bunbougu_keshigomu.png',
+  },
+  {
+    name: 'たこ焼き',
+    price: 420,
+    img: 'https://3.bp.blogspot.com/-lCXpTHWwsX8/VNH6plhYL_I/AAAAAAAArT8/HN85RSAuIZE/s800/takoyaki_fune.png',
+  },
+  {
+    name: 'ソフトクリーム',
+    price: 290,
+    img: 'https://3.bp.blogspot.com/-lPKnoGiDyUI/UWykkn7a0tI/AAAAAAAAQkg/KOCcbK3we4E/s1600/sweets_icecream.png',
+  },
+]
