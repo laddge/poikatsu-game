72d2a1aae993620c1d38969d23055d214b091ea8
Laddge <dev.laddge@gmail.com>
2023-04-30 14:33:31 +0000

    add charge action

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVfm } from 'vue-final-modal'
+import 'vue-final-modal/style.css'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).use(createPinia()).mount('#app')
+createApp(App).use(createPinia()).use(createVfm()).mount('#app')
