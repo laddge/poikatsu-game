52ea2f79ea7a3784b7ae2142d0c4972097467e0e
Laddge <dev.laddge@gmail.com>
2023-04-30 14:46:43 +0000

    sticky footer

--- src/components/views/MainView.vue
+++ src/components/views/MainView.vue
@@ -23,47 +23,34 @@ const totalMoney = computed(() => {
 </script>
 
 <template>
-  <div class="p-4">
-    <div class="text-4xl text-center p-6">
-      {{ date.getMonth() + 1 }} / {{ date.getDate() }} ({{ '日月火水木金土'[date.getDay()] }})
-    </div>
-    <div class="rounded-box bg-base-200 max-w-xl mx-auto mt-3 p-8">
-      <img :src="item.img" :alt="item.img.replace(/.+\/(.+?)\..+(.*)?$/, '$1')" class="h-20 mx-auto my-6" />
-      <div class="text-lg text-center font-bold">
-        <p>{{ item.name }}</p>
-        <p>¥ {{ item.price.toLocaleString() }}</p>
+  <div class="min-h-screen">
+    <div class="p-4">
+      <div class="text-4xl text-center p-6">
+        {{ date.getMonth() + 1 }} / {{ date.getDate() }} ({{ '日月火水木金土'[date.getDay()] }})
       </div>
-      <select class="select w-full mt-6">
-        <option>現金</option>
-      </select>
-      <button class="btn btn-primary w-full mt-2">支払う</button>
+      <div class="rounded-box bg-base-200 max-w-xl mx-auto mt-3 p-8">
+        <img :src="item.img" :alt="item.img.replace(/.+\/(.+?)\..+(.*)?$/, '$1')" class="h-20 mx-auto my-6" />
+        <div class="text-lg text-center font-bold">
+          <p>{{ item.name }}</p>
+          <p>¥ {{ item.price.toLocaleString() }}</p>
+        </div>
+        <select class="select w-full mt-6">
+          <option>現金</option>
+        </select>
+        <button class="btn btn-primary w-full mt-2">支払う</button>
+      </div>
     </div>
-    <div class="fixed left-0 bottom-0 w-full bg-base-100/75 backdrop-blur">
-      <input type="checkbox" id="walletCheck" class="hidden" />
+    <div class="sticky top-[100vh]">
       <div class="flex items-center text-lg px-6 mt-3">
-        <label for="walletCheck" class="flex items-center gap-3 md:hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
-          </svg>
-          Wallet
-        </label>
+        <p>Wallet</p>
         <div class="ml-auto font-bold">
           合計: ¥ {{ totalMoney.toLocaleString() }}
         </div>
       </div>
-      <div class="grid grid-cols-2 md:grid-cols-4 gap-3 p-3 h-0 wallet md:h-auto">
+      <div class="grid grid-cols-2 md:grid-cols-4 gap-3 p-3">
         <PayDeck v-for="pay in wallet" :pay="pay" :cash="cash" :date="date" :item="item" />
         <PayDeck :cash="cash" :date="date" :item="item" />
       </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-#walletCheck:checked + div label svg {
-  @apply rotate-180;
-}
-#walletCheck:checked ~ .wallet {
-  @apply h-auto;
-}
-</style>
